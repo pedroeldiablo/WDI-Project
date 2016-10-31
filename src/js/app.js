b646c9f770f80f5146068f81a6401fac35868868
Peter Williams <pedroeldiablo@yahoo.com>
2016-10-31 16:49:21 +0000

    app.js register form to firstName etc

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,17 @@
 console.log("JS loaded!");
-
 $(() =>{
   let $mapDiv =$('#map');
   console.log($mapDiv);
-
   let map = new google.maps.Map($mapDiv[0], {
     center: { lat: 51.5153, lng: -0.0722 },
     zoom: 16
   });
-
   navigator.geolocation.getCurrentPosition(function(position) {
     let latLng = {
       lat: position.coords.latitude,
       lng: position.coords.longitude,
     };
-
     map.panTo(latLng);
-
     let marker = new google.maps.Marker({
       position: latLng,
       animation: google.maps.Animation.BOUNCE,
@@ -24,7 +19,6 @@ $(() =>{
       map
     });
   });
-
   let $main = $('main');
   $('.register').on('click', showRegisterForm);
   $('.login').on('click', showLoginForm);
@@ -33,7 +27,7 @@ $(() =>{
   $main.on('click', 'button.edit', getUser);
   $('.usersIndex').on('click', getUsers);
   $('.logout').on('click', logout);
-  $('.eventsIndex').on('click', getEvents);
+  $('.sharksIndex').on('click', getSharks);
   function isLoggedIn() {
     return !!localStorage.getItem('token');
   }
@@ -48,16 +42,37 @@ $(() =>{
       <h2>Register</h2>
       <form method="post" action="/register">
         <div class="form-group">
-          <input class="form-control" name="username" placeholder="Username">
+          <input class="form-control" name="firstName" placeholder="First Name">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="lastName" placeholder="Last Name">
         </div>
         <div class="form-group">
           <input class="form-control" name="email" placeholder="Email">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="age" placeholder="Age e.g 21">
         </div>
         <div class="form-group">
           <input class="form-control" type="password" name="password" placeholder="Password">
         </div>
         <div class="form-group">
           <input class="form-control" type="password" name="passwordConfirmation" placeholder="Password Confirmation">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="gender" placeholder="Male or Female?">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="interestedIn" placeholder="Men, Women, or Both?">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="postcode" placeholder="Postcode">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="fact" placeholder="Tell us a quick fact about yourself!">
+        </div>
+        <div class="form-group">
+          <input class="form-control" name="profilePic" placeholder="Upload your image here">
         </div>
         <button class="btn btn-primary">Register</button>
       </form>
@@ -122,22 +137,19 @@ $(() =>{
     .done(showUsers)
     .fail(showLoginForm);
   }
-
-  function getEvents() {
+  function getSharks() {
     if(event) event.preventDefault();
     let token = localStorage.getItem('token');
     $.ajax({
-      url: '/events',
+      url: '/sharks',
       method: "GET",
       beforeSend: function(jqXHR) {
         if(token) return jqXHR.setRequestHeader('Authorization', `Bearer ${token}`);
       }
     })
-    .done(showEvents)
+    .done(showSharks)
     .fail(showLoginForm);
   }
-
-
   function showUsers(users) {
     console.log(users);
     let $row = $('<div class="row"></div>');
@@ -147,7 +159,7 @@ $(() =>{
           <div class="card">
             <img class="card-img-top" src="http://fillmurray.com/300/300" alt="Card image cap">
             <div class="card-block">
-              <h4 class="card-title">${user.username}</h4>
+              <h4 class="card-title">${user.firstName}</h4>
             </div>
           </div>
           <button class="btn btn-danger delete" data-id="${user._id}">Delete</button>
@@ -157,11 +169,10 @@ $(() =>{
     });
     $main.html($row);
   }
-
-  function showEvents(events) {
-    console.log(events);
+  function showSharks(sharks) {
+    console.log(sharks);
     let $row = $('<div class="row"></div>');
-    events.forEach((event) => {
+    sharks.forEach((shark) => {
       $row.append(`
         <div class="col-md-4">
           <div class="card">
@@ -180,8 +191,7 @@ $(() =>{
     });
     $main.html($row);
   }
-  showEvents();
-
+  showSharks();
   function deleteUser() {
     let id = $(this).data('id');
     let token = localStorage.getItem('token');
@@ -213,5 +223,4 @@ $(() =>{
     localStorage.removeItem('token');
     showLoginForm();
   }
-
 });
